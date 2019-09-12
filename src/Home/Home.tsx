0b65ff176832b5b35303857c14e22b67fea81768
Matt <mwells21@uncc.edu>
2019-09-12 13:00:09 -0400

    Start on home page

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
+import {Card, CardContent, CardMedia, Typography} from "@material-ui/core";
+import logo from "../Images/CARLogoPrimary.png";
+import crown from "../Images/UNCCCrown.png";
+import "./Home.scss";
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h2>HELLO</h2>
-                <p>Cras facilisis urna ornare ex volutpat, et
-                convallis erat elementum. Ut aliquam, ipsum vitae
-                gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                metus nec massa. Maecenas hendrerit laoreet augue
-                nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-
-                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            <div style={{backgroundImage: crown}}>
+                <Card className="Card">
+                    <CardMedia
+                        className="Logo"
+                        image={logo}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica 
+                        </Typography>
+                    </CardContent>
+                </Card>
             </div>
         );
     }
