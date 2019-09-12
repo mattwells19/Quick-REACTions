bc7e94111354ab4b4aa01aca4b5876ed9f62e42d
Matt <mwells21@uncc.edu>
2019-09-12 19:50:30 -0400

    Background added, working on content

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,31 +1,20 @@
-import React, { Component } from "react";
-import {Card, CardContent, CardMedia, Typography} from "@material-ui/core";
-import logo from "../Images/CARLogoPrimary.png";
+import React from "react";
+import {Paper, Typography} from "@material-ui/core";
 import crown from "../Images/UNCCCrown.png";
+import logo from "../Images/CARLogoPrimary.png";
 import "./Home.scss";
+import { withStyles } from "@material-ui/styles";
 
-class Home extends Component {
-    render() {
-        return (
-            <div style={{backgroundImage: crown}}>
-                <Card className="Card">
-                    <CardMedia
-                        className="Logo"
-                        image={logo}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Lizard
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                            across all continents except Antarctica 
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </div>
-        );
-    }
+function Home() {
+    return (
+        <div>
+            <img src={crown} className="backdrop" alt="Background Crown"/>
+            <img src={logo} className="logo" alt="Logo"/>
+            <Typography align="center" className="header" variant="h2">
+                Charlotte Area Robotics
+            </Typography>
+        </div>
+    );
 }
 
 export default Home;
