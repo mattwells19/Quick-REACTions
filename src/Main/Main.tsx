e546a387e781a81b721ff8177d497b376e0e85b1
Mama <Mama@Mamas-Air.attlocal.net>
2019-09-18 17:59:02 -0400

    add Main

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,39 +1,51 @@
-import React, { Component } from "react";
-import { Route, NavLink, HashRouter } from "react-router-dom";
-import Home from "../Home/Home";
-import About from "../About/About";
-import Outreach from "../Outreach/Outreach";
-import { AppBar, Toolbar } from "@material-ui/core"
-import "./Main.scss";
-import Sponsors from "../Sponsors/Sponsors";
-import Competition from "../Competition/Competition";
-import Archive from "../Archive/Archive";
+import React, { Component } from "react";
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+import About from "../About/About";
+import Outreach from "../Outreach/Outreach";
+import Sponsors from "../Sponsors/Sponsors";
+import Competition from "../Competition/Competition";
+import Archive from "../Archive/Archive";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-class Main extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <AppBar position="static">
-          <Toolbar className="header">
-            <NavLink to="/" className="NavBarSelection">Home</NavLink>
-            <NavLink to="/about" className="NavBarSelection">About</NavLink>
-            <NavLink to="/sponsors" className="NavBarSelection">Sponsors</NavLink>
-            <NavLink to="/outreach" className="NavBarSelection">Outreach</NavLink>
-            <NavLink to="/competition" className="NavBarSelection">Competition</NavLink>
-            <NavLink to="/archive" className="NavBarSelection">Archive</NavLink>
-          </Toolbar>
-        </AppBar>
-        <div className="content">
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/outreach" component={Outreach} />
-          <Route path="/sponsors" component={Sponsors} />
-          <Route path="/competition" component={Competition} />
-          <Route path="/archive" component={Archive} />
-        </div>
-      </HashRouter>
-    );
-  }
+const theme = createMuiTheme({
+  typography: {
+    h4: {
+      fontFamily: "Inconsolata",
+      fontSize: "1.75rem",
+      fontWeight: 700,
+    },
+    h5: {
+      fontFamily: "Inconsolata",
+      fontSize: "1rem",
+      fontWeight: 700,
+    },
+  },
+});
+
+class Main extends Component {
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <NavBar />
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/outreach" component={Outreach} />
+            <Route path="/sponsors" component={Sponsors} />
+            <Route path="/competition" component={Competition} />
+            <Route path="/archive" component={Archive} />
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+  }
 }
 
-export default Main;
+export default Main;
