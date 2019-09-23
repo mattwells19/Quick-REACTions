f878cef6188f8658f897781ecd7277a89d18b225
Matt <mwells21@uncc.edu>
2019-09-23 14:18:03 -0400

    A start to the about page

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
-
+import { Typography, CardActionArea, CardMedia, CardContent, Card } from "@material-ui/core";
+import ReturningMembers from "../Images/ReturningMembers.jpg";
 class About extends Component {
     render() {
         return (
-            <div>
-                <h2>STUFF</h2>
-                <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-                <ol>
-                    <li>Nulla pulvinar diam</li>
-                    <li>Facilisis bibendum</li>
-                    <li>Vestibulum vulputate</li>
-                    <li>Eget erat</li>
-                    <li>Id porttitor</li>
-                </ol>
+            <div style={{marginTop: "5rem"}}>
+                <Card style={{maxWidth: "800px", margin: "5rem"}}>
+                    <CardActionArea>
+                        <CardMedia
+                            image={ReturningMembers}
+                            style={{height: "350px"}}
+                        />
+                            <CardContent>
+                                <Typography align="center" variant="h4">
+                                    These are our returning members!
+                                </Typography>
+                            </CardContent>
+                    </CardActionArea>
+                </Card>
             </div>
         );
     }
