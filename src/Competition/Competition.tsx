adb2ec90c4fd7b1d1e181783a25ea9fae303e51f
mattwells19 <mwells21@uncc.edu>
2019-11-08 11:19:32 -0500

    Added rules pdf

--- src/Competition/Competition.tsx
+++ src/Competition/Competition.tsx
@@ -5,10 +5,10 @@ import CountdownTimer from "./CountdownTimer";
 class Competition extends Component {
     render() {
         return (
-            <div style={{paddingTop: "8rem"}}>
-                <CountdownTimer/>
+            <div style={{ paddingTop: "8rem" }}>
+                <CountdownTimer />
                 {/* http://www.africau.edu/images/default/sample.pdf */}
-                <iframe title="Rules" src="http://www.africau.edu/images/default/sample.pdf" width="300px" height="600px" />
+                <iframe title="Rules" src="./PiDayRules-Nov04_FINAL.pdf" width="300px" height="600px" />
             </div>
         );
     }
