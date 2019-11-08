5d52cd3dd897c2bb2efb389ab8a660bba5aac642
Matt <mwells21@uncc.edu>
2019-11-07 19:56:45 -0500

    Countdown timer and PDF placeholder

--- src/Competition/Competition.tsx
+++ src/Competition/Competition.tsx
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
+import CountdownTimer from "./CountdownTimer";
+
 
 class Competition extends Component {
     render() {
         return (
-            <div>
-                <h2>STUFF</h2>
-                <p>Mauris sem velit, vehicula eget sodales vitae,
-        rhoncus eget sapien:</p>
-                <ol>
-                    <li>Nulla pulvinar diam</li>
-                    <li>Facilisis bibendum</li>
-                    <li>Vestibulum vulputate</li>
-                    <li>Eget erat</li>
-                    <li>Id porttitor</li>
-                </ol>
+            <div style={{paddingTop: "8rem"}}>
+                <CountdownTimer/>
+                {/* http://www.africau.edu/images/default/sample.pdf */}
+                <iframe title="Rules" src="http://www.africau.edu/images/default/sample.pdf" width="300px" height="600px" />
             </div>
         );
     }
