21b5876edbdee7675fd1feb56820e3578f4e5a47
Matt <mwells21@uncc.edu>
2019-11-08 19:44:12 -0500

    Added rules

--- src/Competition/Competition.tsx
+++ src/Competition/Competition.tsx
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
 import CountdownTimer from "./CountdownTimer";
+import { Paper, withStyles, Typography, Divider } from "@material-ui/core";
 
+const RulesBackground = withStyles({
+    root: {
+        backgroundColor: "#212B31",
+        width: "max-content",
+        padding: "2rem",
+        margin: "2rem auto"
+    },
+})(Paper);
+
+const RulesText = withStyles({
+    root: {
+        color: "#B3A369",
+    },
+})(Typography);
 
 class Competition extends Component {
     render() {
         return (
             <div style={{ paddingTop: "8rem" }}>
                 <CountdownTimer />
-                {/* http://www.africau.edu/images/default/sample.pdf */}
-                <iframe title="Rules" src="./PiDayRules-Nov04_FINAL.pdf" width="300px" height="600px" />
+                <RulesBackground>
+                    <RulesText variant="h2" align="center">
+                        The Rules: Pi Day 2020
+                    </RulesText>
+                    <Divider style={{ backgroundColor: "#B3A369", margin: "1rem"}} variant="middle" />
+                    <iframe title="rules" src="https://drive.google.com/file/d/1rc0wpz5IDh3fPyaSZwz_TqPAv9dnhEcB/preview" width="1024" height="768"></iframe>
+                </RulesBackground>
             </div>
         );
     }
