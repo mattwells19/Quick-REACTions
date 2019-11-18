42bd8dff5fe7dbdc6f21d31f3b3cbda8fce70dcc
Matt <mwells21@uncc.edu>
2019-11-17 19:09:15 -0500

    Renaming

--- src/Competition/Competition.tsx
+++ src/Competition/Competition.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import CountdownTimer from "./CountdownTimer";
 import { Paper, withStyles, Typography, Divider } from "@material-ui/core";
 
-const RulesBackground = withStyles({
+const PaperBackground = withStyles({
     root: {
         backgroundColor: "#212B31",
         width: "max-content",
@@ -22,13 +22,13 @@ class Competition extends Component {
         return (
             <div style={{ paddingTop: "8rem" }}>
                 <CountdownTimer />
-                <RulesBackground>
+                <PaperBackground>
                     <RulesText variant="h2" align="center">
                         The Rules: Pi Day 2020
                     </RulesText>
                     <Divider style={{ backgroundColor: "#B3A369", margin: "1rem"}} variant="middle" />
                     <iframe title="rules" src="https://drive.google.com/file/d/1rc0wpz5IDh3fPyaSZwz_TqPAv9dnhEcB/preview" width="1024" height="768"></iframe>
-                </RulesBackground>
+                </PaperBackground>
             </div>
         );
     }
